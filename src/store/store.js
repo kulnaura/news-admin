9df289da3af3eb8cfc9c57f75f0fe6a544caceac
alexander <kulnaura@gmail.com>
2018-04-20 09:11:56 +0300

    add users login/pass to last news

--- src/store/store.js
+++ src/store/store.js
@@ -18,7 +18,7 @@ export const store = new Vuex.Store({
             {id: 2, title: "The news Two", description: "The news Two description", timeCreated: 1523939355267, timeUpdated: 1523939355267, published: false},
             {id: 3, title: "The news Three", description: "The news Three description", timeCreated: 1524039355267, timeUpdated: 1524039355267, published: true},
             {id: 4, title: "The news Four", description: "The news Four description", timeCreated: 1524049355267, timeUpdated: 1524049355267, published: false},
-            {id: 5, title: "The news Five", description: "The news Five description", timeCreated: 1524059355267, timeUpdated: 1524059355267, published: true},
+            {id: 5, title: "The news Five", description: "Users: user/qwerty admin/admin123", timeCreated: 1524059355267, timeUpdated: 1524059355267, published: true},
         ],
         users: [
             {login: "admin", password: "admin123", admin: true},
